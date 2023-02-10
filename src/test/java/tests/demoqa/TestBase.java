fd88a9ab01a98939b51e8dde46d96a4b0320f93c
petr <panisimov@gmail.com>
2023-02-10 23:21:57 +0800

    added Allure attachments

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -1,13 +1,27 @@
 package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
 
     @BeforeAll
     static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "http://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        //пробую вызвать удаленный браузер из селенида
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshots");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 }

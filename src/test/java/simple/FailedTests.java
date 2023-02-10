36aae4c185a10bc5b0a1ea7ab13871d815d1c890
petr <panisimov@gmail.com>
2023-02-10 22:16:40 +0800

    commit with simple tests

---
+++ src/test/java/simple/FailedTests.java
@@ -0,0 +1,37 @@
+package simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FailedTests {
+    @Test
+    void test00() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test01() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test02() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test03() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test04() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test05() {
+        assertTrue(false);
+    }
+}
---
+++ src/test/java/simple/SkippedTests.java
@@ -0,0 +1,27 @@
+package simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+    @Test
+    @Disabled
+    void test00() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void test01() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled("some reason")
+    void test02() {
+        assertTrue(false);
+    }
+
+}
